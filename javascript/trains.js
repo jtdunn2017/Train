7a38a3c61cbf36058f8825e2a8502946cf6c9ffc
Jeff Jordan <jtdunn2017@outlook.com>
2017-12-20 21:39:30 -0500

    most html working + most of logic is in place

---
+++ javascript/trains.js
@@ -0,0 +1,14 @@
+
+<script src="https://www.gstatic.com/firebasejs/4.8.1/firebase.js"></script>
+<script>
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyCGRXvD88P3ZcGPBE4mgYjK-zMGZ3FNTtE",
+    authDomain: "train-schedules-5b28f.firebaseapp.com",
+    databaseURL: "https://train-schedules-5b28f.firebaseio.com",
+    projectId: "train-schedules-5b28f",
+    storageBucket: "train-schedules-5b28f.appspot.com",
+    messagingSenderId: "486424654375"
+  };
+  firebase.initializeApp(config);
+</script>
